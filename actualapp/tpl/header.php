272db51d78ede9dcce0e6ffad0703b54fe576778
ernestromen <ernestromen1996@gmail>
2024-02-20 20:53:12 +0200

    Added meta tag to allow https linke to work

--- actualapp/tpl/header.php
+++ actualapp/tpl/header.php
@@ -28,6 +28,7 @@ $main_url = "http://" . $_SERVER['SERVER_NAME'];
       <!-- Tweaks for older IEs-->
       <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
    </head>
    <!-- body -->
    <body class="main-layout">
